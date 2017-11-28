a4c06fadf277349afd2688f84554ff5d59945223
Kavi <kavi@sas.upenn.edu>
2017-11-28 02:42:43 -0500

    8

--- index.js
+++ index.js
@@ -15,11 +15,52 @@ app.get("/", function (req, res) {
 });
 
 
-// var processPostback = function (event) {
-//   var senderId = event.sender.id;
-//   var payload = event.postback.payload;
-//   res.send("processed");
-// }
+var processPostback = function (event) {
+  var senderId = event.sender.id;
+  var payload = event.postback.payload;
+  res.send("processed");
+
+  if (payload === "Greeting") {
+    // Get user's first name from the User Profile API
+    // and include it in the greeting
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function (error, response, body) {
+      var greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " +  error);
+      } else {
+        var bodyObj = JSON.parse(body);
+        name = bodyObj.first_name;
+        greeting = "Hi " + name + ". ";
+      }
+      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      sendMessage(senderId, {text: message});
+    });
+  }
+}
+
+// sends message to user
+var sendMessage = function (recipientId, message) {
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: "POST",
+    json: {
+      recipient: {id: recipientId},
+      message: message,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log("Error sending message: " + response.error);
+    }
+  });
+}
 
 
 app.post('/webhook', function (req, res) {  
@@ -30,9 +71,7 @@ app.post('/webhook', function (req, res) {
 
       entry.messaging.forEach(function (event) {
         if (event.postback) {
-          // processPostback(event);
-          console.log("processing");
-          res.send("processed");
+          processPostback(event);
         }
       });
     });
